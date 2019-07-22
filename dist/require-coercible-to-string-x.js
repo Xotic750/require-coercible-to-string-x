625a476b38c982aadd0a99ce5dade1ef33b605b1
Graham Fairweather <xotic750@gmail.com>
2019-07-22 14:11:43 +0200

    :bookmark: v2.0.18
    Dependency updates

--- dist/require-coercible-to-string-x.js
+++ dist/require-coercible-to-string-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-07-21T22:15:56.748Z",
+  "date": "2019-07-22T12:11:38.471Z",
   "describe": "",
   "description": "Requires an argument is corecible then converts using ToString.",
   "file": "require-coercible-to-string-x.js",
   "hash": "1d9f82a110123225e99f",
   "license": "MIT",
-  "version": "2.0.17"
+  "version": "2.0.18"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
